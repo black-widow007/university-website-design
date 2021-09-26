964bc3f72e9aacb3d4f934cbdd73eac2c18b3235
Catherine Powers <cnpowers2102@gmail.com>
2021-09-26 10:15:13 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const navLinks = document.getElementById("navLinks");
+
+// Show & Hide Menu by styling right css value of nav links
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-400px";
+}
+
